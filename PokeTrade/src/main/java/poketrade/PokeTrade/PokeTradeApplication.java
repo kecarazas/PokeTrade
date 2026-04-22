b45da222dede4c5c612984e9e0eeb90dc620d07e
kecarazas <ke.carazas@duocuc.cl>
2026-04-22 11:16:56 -0400

    subida del proyecto base a GitHub

---
+++ PokeTrade/src/main/java/poketrade/PokeTrade/PokeTradeApplication.java
@@ -0,0 +1,13 @@
+package poketrade.PokeTrade;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PokeTradeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PokeTradeApplication.class, args);
+	}
+
+}
