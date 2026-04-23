3f46cfba9a4cb2d088dc2d874499dd1e7cc77721
kecarazas <ke.carazas@duocuc.cl>
2026-04-22 20:24:11 -0400

    llenado los repository

--- PokeTrade/src/main/java/poketrade/PokeTrade/repository/CartaRepository.java
+++ PokeTrade/src/main/java/poketrade/PokeTrade/repository/CartaRepository.java
@@ -1,4 +1,9 @@
 package poketrade.PokeTrade.repository;
 
-public interface CartaRepository {
+import poketrade.PokeTrade.model.Carta;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CartaRepository extends JpaRepository<Carta, Long> {
 }
--- PokeTrade/src/main/java/poketrade/PokeTrade/repository/PublicacionRepository.java
+++ PokeTrade/src/main/java/poketrade/PokeTrade/repository/PublicacionRepository.java
@@ -1,4 +1,9 @@
 package poketrade.PokeTrade.repository;
 
-public interface PublicacionRepository {
+import poketrade.PokeTrade.model.Publicacion;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PublicacionRepository extends JpaRepository<Publicacion, Long> {
 }
