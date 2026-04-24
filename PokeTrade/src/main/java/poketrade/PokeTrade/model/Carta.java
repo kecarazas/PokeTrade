9557a1834ead1cf06e83975a5ce53b596844649c
brhuenten <br.huenten@duocuc.cl>
2026-04-23 23:05:38 -0400

    Dependencia Bean Validation y validaciones agregadas en clases

--- PokeTrade/src/main/java/poketrade/PokeTrade/model/Carta.java
+++ PokeTrade/src/main/java/poketrade/PokeTrade/model/Carta.java
@@ -1,6 +1,10 @@
 package poketrade.PokeTrade.model;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.AllArgsConstructor;
@@ -16,35 +20,53 @@ public class Carta {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
+    @NotBlank(message = "Debe existir un nombre")
+    @Size(max = 25, message = "El nombre no puede superar los 25 caracteres")
     @Column(nullable=false)
     private String nombre;
 
+    @Min(value = 1, message = "Los PS deben ser mayores a 0")
+    @Max(value = 340, message = "Los PS no deben superar los 340")
     private int ps;
 
+    @NotBlank(message = "Debe tener un tipo")
+    @Size(max = 20, message = "El tipo no puede superar los 20 caracteres")
     @Column(nullable=false)
     private String tipo;
 
+    @Size(max = 35, message = "La habilidad no puede superar los 35 caracteres")
     private String habilidad;
 
+    @Size(max = 100, message = "La descripción de habilidad no puede superar los 100 caracteres")
     @Column(length=100)
     private String descripcionHabilidad;
 
+    @Min(value = 0, message = "El coste de energía no puede ser negativo")
+    @Max(value = 8, message = "El coste de energia debe ser inferior a 8")
     private int costeEnergia;
 
+    @Min(value = 0, message = "La retirada no puede ser negativa")
     private int retirada;
 
+    @Size(max = 35, message = "El ataque no puede superar los 35 caracteres")
     private String ataque;
 
+    @Min(value = 0, message = "El daño del ataque no puede ser negativo")
+    @Max(value = 400, message = "El daño del ataque no deben superar los 400")
     private int danoAtaque;
 
+    @Size(max = 100, message = "La descripción del ataque no puede superar los 100 caracteres")
     @Column(length=100)
     private String descripcionAtaque;
 
+    @NotBlank(message = "Debe existir una debilidad")
     @Column(nullable = false)
     private String debilidad;
 
+    @Min(value = 0, message = "La resistencia no puede ser negativa")
     private int resistencia;
 
+    @NotBlank(message = "Debe existir una rareza")
     @Column(nullable=false)
     private String rareza;
 
--- PokeTrade/src/main/java/poketrade/PokeTrade/model/Usuario.java
+++ PokeTrade/src/main/java/poketrade/PokeTrade/model/Usuario.java
@@ -1,5 +1,8 @@
 package poketrade.PokeTrade.model;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.AllArgsConstructor;
@@ -16,9 +19,15 @@ public class Usuario {
     @GeneratedValue(strategy=GenerationType.IDENTITY)
     private Integer id;
 
+    @NotBlank(message = "Debe existir el nombre de usuario")
+    @Size(min = 3, max = 30, message = "El username debe tener entre 3 y 30 caracteres")
     @Column(nullable=false, unique=true)
     private String username;
 
+    @NotBlank(message = "Debe existir un correo")
+    @Email(message = "El correo debe tener un formato válido")
+    //Esta validacion es para ver si el correo ingresado tiene el formato de correo apropiado por ejemplo: usuario@correo.com/.cl
+    @Size(max = 100, message = "El correo no puede superar los 100 caracteres")
     @Column(nullable=false, unique=true)
     private String correo;
 }
