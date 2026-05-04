f1d4d2911f6f280f4c8510f092e6303e3edfca04
kecarazas <ke.carazas@duocuc.cl>
2026-05-03 21:22:17 -0400

    se implemento el archivo sql para insertar el usuario TIENDA, se implemento el archivo sql para insertar 3 publicaciones

---
+++ PokeTrade/src/main/resources/db/migration/V4__insertar_datos_usuario.sql
@@ -0,0 +1,4 @@
+INSERT INTO usuario
+(username, nombre, apellido, email, password)
+VALUES
+    ('TIENDA', 'kevin', 'carazas', 'kevin@gmail.com', '2004kevin')
---
+++ PokeTrade/src/main/resources/db/migration/V5__insertar_datos_publicacion.sql
@@ -0,0 +1,7 @@
+INSERT INTO publicacion
+(precio, stock, tipo_vendedor, carta_id, usuario_id)
+VALUES
+    (39.999, 10, 'TIENDA', 6, 1),
+    (19.990, 10, 'TIENDA', 25, 1),
+    (9.999, 15, 'TIENDA', 68, 1)
+
