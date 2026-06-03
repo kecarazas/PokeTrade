bbb261a7f1c454ab7219744204e15ade71b1bbe4
kecarazas <ke.carazas@duocuc.cl>
2026-06-02 22:40:33 -0400

    Agregar usuario-services

---
+++ usuario-services/src/main/java/poketrade/usuario_services/UsuarioServicesApplication.java
@@ -0,0 +1,13 @@
+package poketrade.usuario_services;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsuarioServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UsuarioServicesApplication.class, args);
+	}
+
+}
