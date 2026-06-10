bd2068ba3c5d99fde9255dbc633bcc8b35ef4d2a
kecarazas <ke.carazas@duocuc.cl>
2026-06-09 20:37:22 -0400

    se modifico usuarioClient para que use una variable de entorno en la url de feignClient

--- PokeTrade/src/main/java/poketrade/PokeTrade/cliente/UsuarioClient.java
+++ PokeTrade/src/main/java/poketrade/PokeTrade/cliente/UsuarioClient.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 
 @FeignClient(
         name = "usuario-service",
-        url = "http://localhost:8081"
+        url = "${usuario-service.url}"
 )
 public interface UsuarioClient {
 
