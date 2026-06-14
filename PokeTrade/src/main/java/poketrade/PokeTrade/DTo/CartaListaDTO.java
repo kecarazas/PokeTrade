76288ced1b4aec4991a811df3ec4c42e397458bf
kecarazas <ke.carazas@duocuc.cl>
2026-06-13 23:33:45 -0400

    me falto agregar el mensaje en @NotEmpty

--- PokeTrade/src/main/java/poketrade/PokeTrade/DTo/CartaListaDTO.java
+++ PokeTrade/src/main/java/poketrade/PokeTrade/DTo/CartaListaDTO.java
@@ -8,7 +8,7 @@ import java.util.List;
 
 @Data
 public class CartaListaDTO {
-    @NotEmpty
+    @NotEmpty(message = "La lista de cartas no puede estar vacía")
     @Valid
     public List<CartaDTo> listaCartas;
 }
