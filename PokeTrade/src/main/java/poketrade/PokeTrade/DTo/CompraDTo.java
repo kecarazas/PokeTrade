39ada1148b673724cbea8f3c02f00ddb48d9eb00
kecarazas <ke.carazas@duocuc.cl>
2026-06-14 16:04:52 -0400

    implemetancion de @Schema en el dto de compra para Swagger

--- PokeTrade/src/main/java/poketrade/PokeTrade/DTo/CompraDTo.java
+++ PokeTrade/src/main/java/poketrade/PokeTrade/DTo/CompraDTo.java
@@ -1,5 +1,6 @@
 package poketrade.PokeTrade.DTo;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.*;
 import jakarta.validation.constraints.*;
 
@@ -10,11 +11,14 @@ public class CompraDTo {
 
     @NotNull(message = "Debe existir una cantidad")
     @Min(value = 1, message = "La cantidad debe ser al menos de 1")
+    @Schema(description = "cantidad para comprar", examples = "1")
     private Integer cantidad;
 
     @NotNull(message = "Debe existir el usuario")
+    @Schema(description = "username necesario para comprar", examples = "TIENDA")
     private String username;
 
     @NotNull(message = "Debe existir la publicacion")
+    @Schema(description = "ID de la publicacion que se quiera comprar", examples = "1")
     private Integer publicacionId;
 }
