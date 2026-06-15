72baab4c9a39916fe3bf1e2c3c32a4d0f6f45c58
kecarazas <ke.carazas@duocuc.cl>
2026-06-14 22:52:40 -0400

    se quitan las mejoras

--- PokeTrade/src/main/java/poketrade/PokeTrade/config/SwaggerConfig.java
+++ PokeTrade/src/main/java/poketrade/PokeTrade/config/SwaggerConfig.java
@@ -2,18 +2,15 @@ package poketrade.PokeTrade.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
-import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
-
-import java.util.List;
 
 
 @Configuration
 public class SwaggerConfig {
 
-    public OpenAPI baseOpenAPI() {
+    @Bean
+    public OpenAPI customOpenAPI() {
         return new OpenAPI()
                 .info(new Info()
                         .title("PokeTrade API")
@@ -21,18 +18,4 @@ public class SwaggerConfig {
                         .description("Documentacion de la API para el sistema de compra, venta y tradeo de cartas pokemon"));
     }
 
-    @Bean
-    @Profile("dev")
-    public OpenAPI devOpenAPI() {
-        return baseOpenAPI();
-    }
-
-    @Bean
-    @Profile("prod")
-    public OpenAPI prodOpenAPI() {
-        return baseOpenAPI()
-                .servers(List.of(
-                        new Server().url("https://poketrade-production.up.railway.app").description("Servidor de produccion")
-                ));
-    }
 }
